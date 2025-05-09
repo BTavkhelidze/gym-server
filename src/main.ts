2f4573eb996a8ba95cfb121f85f3efcb206c6c8a
BTavkhelidze <beqatavkhelidze4@gmail.com>
2025-05-10 03:41:44 +0400

    change front url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://gym-front-seven.vercel.app',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 8080);
 }
