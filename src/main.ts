c8fd3ff2582e1138a6f2ed03ff1808f4e076f94f
BTavkhelidze <beqatavkhelidze4@gmail.com>
2025-05-13 23:02:39 +0400

    improve/ main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://gym-front-seven.vercel.app',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  origin: 'https://gym-front-seven.vercel.app',
+  credentials: true,
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  exposedHeaders: ['set-cookie']
+});
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 8080);
